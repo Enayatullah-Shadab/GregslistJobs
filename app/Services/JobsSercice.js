cb0648d01721c7b958ab41eab13f9518714cd2f9
Enayatullal Shadab <Enayatullah_Shadab@yahoo.com>
2021-07-15 00:19:29 -0600

    JobMethod

--- app/Services/JobsSercice.js
+++ app/Services/JobsSercice.js
@@ -1,5 +1,10 @@
 //gets request/information from controller that applies to the business logic, and based on that return certain data to store 
-class JobsService { }
+class JobsService {
+    createJob(rawJob) {
+
+        ProxyState.jobs = [...ProxyState.jobs, new Job(rawJob)]
+    }
+}
 
 
 // Singleton Only one instance is ever made and the same instance is always exported
