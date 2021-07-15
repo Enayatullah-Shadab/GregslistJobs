ddc545376dd07b869e8960c11d4e694f89f27998
Enayatullal Shadab <Enayatullah_Shadab@yahoo.com>
2021-07-15 01:04:09 -0600

    fix

--- app/Controllers/JobsController.js
+++ app/Controllers/JobsController.js
@@ -1,11 +1,19 @@
 //NOTE controller takes user input from view(html) and calls to the model or service and attempt to change  
 import { ProxyState } from "../AppState.js";
 import Job from "../Models/Job.js";
+//encapsulation: hiding the internal representation or state of an object from outside manipulation 
 
+
+function _draw() {
+    let template = "";
+    let Jabs = ProxyState.Jabs.forEach(jab => {
+        template += Job.Template
+    })
+    document.getElementById('jobs').innerHTML = template
+}
 
 // class is the blue print of an object and it define what a thing is.
 export default class JobsController {
-
     constructor() {
         // When 'jobs' changes in the AppState run the _draw method
         ProxyState.on('jobs', _draw)
@@ -13,17 +21,19 @@ export default class JobsController {
 
         // This only runs when the app first loads because data is already in the state
         _draw()
-
     }
-
-
-
-}//encapsulation: hiding the internal representation or state of an object from outside manipulation 
-function _draw() {
-    let template = "";
-    let Jabs = ProxyState.Jabs.forEach(jab => {
-        template += Job.Template
-    })
-    document.getElementById('jobs').innerHTML = template
-
+    createJob() {
+        event.preventDefault()
+        let form = event.target
+        let rawJob = {
+            position: form.position.value,
+            location: form.location.value,
+            jobDescription: form.jobDescription.value,
+            qualification: form.qualification.value,
+            wage: form.wage.value,
+        }
+        carsService.createJob(rawJob)
+        form.reset()
+    }
 }
+
