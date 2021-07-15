2fedee092182e52efafbe99d660d71e4eeb5e61d
Enayatullal Shadab <Enayatullah_Shadab@yahoo.com>
2021-07-15 00:21:49 -0600

    ProxyState

--- app/Controllers/JobsController.js
+++ app/Controllers/JobsController.js
@@ -6,9 +6,13 @@ import Job from "../Models/Job.js";
 // class is the blue print of an object and it define what a thing is.
 export default class JobsController {
 
-
     constructor() {
+        // When 'jobs' changes in the AppState run the _draw method
+        ProxyState.on('jobs', _draw)
+        ProxyState.on('jobs', () => { console.log('new jobs') })
 
+        // This only runs when the app first loads because data is already in the state
+        _draw()
 
     }
     // methods are functions that are tied to specific objects.
